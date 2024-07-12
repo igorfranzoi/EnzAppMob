0a5b0cbc91c98799fcdd2cadae61a34c451d448a
igorfranzoi1 <igorfranzoi@yahoo.com.br>
2024-07-11 21:17:08 -0300

    Chamada da tela inicial.

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,27 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"enzappmob/internal/ui"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "enzappmob",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Aplicação para conexão com ERP",
+	Long:  `Disponibilizar funcionalidades do ERP no App.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+
+	Run: func(cmd *cobra.Command, args []string) {
+		ui.InitializeApp() // Função que configura e inicia a aplicação Fyne
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +44,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
