1d7863ab0d7dd849f7fb4c35a7ad521b6b676e6c
igorfranzoi1 <igorfranzoi@yahoo.com.br>
2024-07-11 21:14:45 -0300

    Separação das telas do app e do controle principal.

---
+++ internal/ui/mainwindow.go
@@ -0,0 +1,29 @@
+package ui
+
+import (
+	"enzappmob/internal/ui/login"
+	"fmt"
+
+	"fyne.io/fyne/app"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+	"gopkg.in/yaml.v2"
+)
+
+var appVersion string
+
+func InitializeApp() {
+
+	// Determina o idioma a ser utilizado (default: inglês)
+	bundle := i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("yaml", yaml.Unmarshal)
+
+	//Constrói o app inicial, utilizado por todas as aplicaçãoes
+	enzTechApp := app.New()
+
+	loginReturn := login.LoginScreen(&enzTechApp, bundle)
+
+	if loginReturn {
+		fmt.Printf("app-version %s", appVersion)
+	}
+}
