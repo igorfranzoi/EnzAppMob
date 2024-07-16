9882405dc4cd6e813e571d31b0af5488ca131880
igorfranzoi1 <igorfranzoi@yahoo.com.br>
2024-07-15 21:45:04 -0300

    Mensagens.

--- internal/utils/languagedefine.go
+++ internal/utils/languagedefine.go
@@ -1,48 +1,80 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/text/language"
 	"gopkg.in/yaml.v2"
+)
+
+var (
+	en_us_message = "locales/en-us/screens/login.yaml"
+	pt_br_message = "locales/pt-br/screens/login.yaml"
 )
 
 // Carrega os arquivos de mensagens/language
 func LoadMessages(bundle *i18n.Bundle, filename string) error {
 	file, err := os.Open(filename)
+
 	if err != nil {
+		fmt.Print("primeiro if")
 		return err
 	}
 	defer file.Close()
 
 	// Decodifica o arquivo YAML em um mapa genérico
-	msgs := make(map[string]interface{})
-	if err := yaml.NewDecoder(file).Decode(&msgs); err != nil {
+	msgMap := make(map[string]interface{})
+
+	if err := yaml.NewDecoder(file).Decode(&msgMap); err != nil {
+		fmt.Print("segundo if")
 		return err
 	}
 
-	// Adiciona as mensagens ao bundle usando o pacote catalog
-	/*for key, value := range msgs {
+	// Incluir as mensagens ao bundle
+	for key, value := range msgMap {
+
+		var defineMessage i18n.Message
+
 		switch msg := value.(type) {
 		case string:
 			// Mensagem simples
-			bundle.AddMessages(language.English, &catalog.Message{
-				ID:   key,
-				One:  msg,
-				Zero: msg, // Pode ser necessário definir Zero se aplicável
-			})
+			defineMessage.ID = key
+			defineMessage.One = msg
+			defineMessage.Zero = msg
+
+			bundle.AddMessages(language.English, &defineMessage)
+
 		case map[interface{}]interface{}:
 			// Mensagem com pluralização
-			one := msg["one"].(string)
-			other := msg["other"].(string)
-			bundle.AddMessages(language.English, &catalog.Message{
-				ID:    key,
-				One:   one,
-				Other: other,
-			})
+			defineMessage.ID = key
+			defineMessage.One = msg["one"].(string)
+			defineMessage.Zero = msg["other"].(string)
+
+			bundle.AddMessages(language.English, &defineMessage)
+
 		default:
 			return fmt.Errorf("unsupported message format for key %s", key)
 		}
+	}
+
+	return nil
+}
+
+// Inicializar as mensagens
+func InitMessages(bundle *i18n.Bundle /*, filename string*/) error {
+
+	filename := en_us_message
+
+	// Carrega arquivos de mensagens
+	if err := LoadMessages(bundle, filename); err != nil {
+		log.Warn().Msg(fmt.Sprintf("Error loading messages: %v\n ", err))
+	}
+
+	/*if err := utils.LoadMessages(bundle, pt_br_message); err != nil {
+		log.Warn().Msg(fmt.Sprintf("Error loading messages: %v\n", err))
 	}*/
 
 	return nil
